caec9631ef5aad0bc33014f3e2c223d2646fea09
Christian Frei <Christian.Frei@me.com>
2017-07-18 17:36:00 +0200

    Finish!

--- ShinyApplication/ui.R
+++ ShinyApplication/ui.R
@@ -9,37 +9,42 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Parabola"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("stretchCompress",
-                   "Stretch(>1.0) or compress(<1.0):",
-                   min = 0.2,
-                   max = +1.8,
-                   step = 0.2,
-                   value = 1),
-       sliderInput("xIntercept",
-                   "X Intercept:",
-                   min = -10,
-                   max = 10,
-                   step = 0.5,
-                   value = 0),
-       sliderInput("yIntercept",
-                   "Y Intercept:",
-                   min = -10,
-                   max = +10,
-                   step = 0.5,
-                   value = 0)
-    ),
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+
+# Define UI for application
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Parabola"),
+   
+   # Sidebar with a slider input
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("stretchCompress",
+                     "Stretch(>1.0) or compress(<1.0):",
+                     min = 0.2,
+                     max = +1.8,
+                     step = 0.2,
+                     value = 1),
+         sliderInput("xIntercept",
+                     "X Intercept:",
+                     min = -10,
+                     max = 10,
+                     step = 0.5,
+                     value = 0),
+         sliderInput("yIntercept",
+                     "Y Intercept:",
+                     min = -10,
+                     max = +10,
+                     step = 0.5,
+                     value = 0)
+      ),
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+
+# Draws UI
+shinyUI(ui)
